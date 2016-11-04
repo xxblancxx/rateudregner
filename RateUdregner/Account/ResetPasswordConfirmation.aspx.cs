e9ccd4222d3a82ce38e1527e97b84a49d02babc4
Marti <Marti@DESKTOP-AEDJOD2>
2016-11-04 14:34:36 +0100

    Add project files.

---
+++ RateUdregner/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace RateUdregner.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ RateUdregner/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RateUdregner.Startup))]
+namespace RateUdregner
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
